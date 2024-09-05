0038d544cf795819a55c4bba39e871a4726ca40c
dikshya-rai <dxyarai10@gmail.com>
2024-09-05 15:44:25 +0545

    Travel and tourism

---
+++ style.js
@@ -0,0 +1,13 @@
+function showAlert() {
+    alert("Start your adventure with us!");
+}
+
+// Optionally, you can add smooth scroll functionality:
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
